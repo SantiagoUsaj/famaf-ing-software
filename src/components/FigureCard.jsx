b6db89ec5d8dd111698026a6233856bfc3e8a71e
Santiago Usaj <santiago.usaj@mi.unc.edu.ar>
2024-09-26 13:33:44 -0300

    BCARE-57 add first idea

---
+++ src/components/FigureCard.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Card, List } from "antd";
+
+const FigureCard = () => {
+  const [data, setData] = useState([
+    {
+      title: "Title 1",
+    },
+    {
+      title: "Title 2",
+    },
+    {
+      title: "Title 3",
+    },
+  ]);
+
+  const handleCardClick = (title) => {
+    console.log(`Card with title ${title} clicked`);
+    setData(data.filter((item) => item.title !== title));
+  };
+
+  const getImageForTitle = (title) => {
+    switch (title) {
+      case "Title 1":
+        return cambio;
+      case "Title 2":
+        return mas2;
+      case "Title 3":
+        return mas4;
+      default:
+        return cambio;
+    }
+  };
+  return (
+    <List
+      grid={{
+        gutter: 0,
+        column: 3,
+      }}
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item style={{ display: "flex", justifyContent: "center" }}>
+          <Card
+            title={item.title}
+            onClick={() => handleCardClick(item.title)}
+            hoverable
+            style={{ width: 300, height: 200 }} // Adjust the width and height as needed
+            cover={<img alt={item.title} src={getImageForTitle(item.title)} />} // Add your image path here
+          >
+            Card content
+          </Card>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+export default FigureCard;
