52a73da5211287c764755ffb4bc2fff8fe5ae187
santiafonso <satiago.afonso@mi.unc.edu.ar>
2024-10-15 19:09:10 -0300

    cambios

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/santiafonso/Documents/prueba/backend/.venv"
+set -gx VIRTUAL_ENV "/home/santiafonso/Documents/demo/backend/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
